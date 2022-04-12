33253398c848935899ef962456caf692b1aa5eeb
Anne.Sinae Yu <anne.sinae@gmail.com>
2022-04-12 14:46:54 +0900

    기본패키지 설치, 폴더구조, axios서버통신

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Hello Anne</h1>
+</template>
+
+<script>
+import apiBoard from "@/api/board";
+export default {
+  name: 'Home-main',
+  mounted() {
+    apiBoard.getArticle(1)
+      .then((response) => {
+          console.log("getArticle", response);
+          this.articles = response.data;
+      })
+      .catch((e) => {
+          console.log(e);
+      });
+
+    
+    apiBoard.getArticles(0)
+      .then((response) => {
+          console.log("getArticles", response);
+          this.articles = response.data;
+      })
+      .catch((e) => {
+          console.log(e);
+      });
+
+    apiBoard.postArticle("userId", "title", "body")
+      .then(function (response) {
+        console.log("postArticle", response);
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+      
+  },
+}
+</script>
